ba98304b651719fb55bcd110da013bf8f25eecde
Eduardo Guadalupe Méndez Miranda <lalorayado1@live.com.mx>
2025-02-13 16:48:36 -0600

    Changed database

--- Database/database_script.py
+++ Database/database_script.py
@@ -1,14 +1,88 @@
 import requests
 from datetime import datetime
-from pprint import pprint as pp
-import logging
+from mariadb import connect
+from mariadb.connections import Connection
+from dotenv import load_dotenv
+import os
 import json
 from typing import List
-import sqlite3
 from utils import logger
 
-con = sqlite3.connect('ram.db')
-cursor = con.cursor()
+load_dotenv()
+
+
+def database_connection(db_created=False) -> Connection:
+    args = {
+        'host': os.getenv('DB_HOST'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PW'),
+    }
+    if db_created:
+        args['database'] = 'ram'
+    connection = connect(**args)
+    return connection
+
+
+def create_database(connection: Connection):
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ram")
+
+
+def create_tables(connection: Connection):
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS episodes(
+            id INT NOT NULL,
+            name VARCHAR(70) NOT NULL,
+            air_date VARCHAR(30) NOT NULL,
+            episode VARCHAR(6) NOT NULL,
+            url VARCHAR(50) NOT NULL,
+            created DATETIME NOT NULL,
+            PRIMARY KEY (id)
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS locations(
+            id INT NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            type VARCHAR(30),
+            dimension VARCHAR(30),
+            url VARCHAR(50) NOT NULL,
+            created DATETIME NOT NULL,
+            PRIMARY KEY (id)
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS characters(
+            id INT NOT NULL,
+            name VARCHAR(60) NOT NULL,
+            status VARCHAR(15) NOT NULL,
+            species VARCHAR(30) NOT NULL,
+            type VARCHAR(20),
+            gender VARCHAR(15) NOT NULL,
+            image VARCHAR(50) NOT NULL,
+            url VARCHAR(50) NOT NULL,
+            created DATETIME NOT NULL,
+            origin_id INT,
+            location_id INT,
+            PRIMARY KEY (id),
+            FOREIGN KEY (origin_id) REFERENCES locations(id) ON DELETE CASCADE,
+            FOREIGN KEY (location_id) REFERENCES locations(id) ON DELETE CASCADE
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS episodes_characters(
+            id INT NOT NULL,
+            episode_id INT NOT NULL,
+            character_id INT NOT NULL,
+            FOREIGN KEY (episode_id) REFERENCES episodes(id) ON DELETE CASCADE,
+            FOREIGN KEY (character_id) REFERENCES characters(id) ON DELETE CASCADE
+        )
+    """
+    )
 
 
 def extract_data() -> List[List]:
@@ -34,117 +108,33 @@ def extract_data() -> List[List]:
     return main_list
 
 
-@logger
-def create_database():
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS episodes(
-            id INTEGER PRIMARY KEY,
-            name STRING,
-            air_date STRING,
-            episode STRING,
-            url STRING,
-            created DATETIME
-        )
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS characters(
-            id INTEGER PRIMARY KEY,
-            name STRING,
-            status STRING,
-            species STRING,
-            type STRING,
-            gender STRING,
-            image STRING,
-            url STRING,
-            created DATETIME,
-            origin_id INTEGER,
-            location_id INTEGER
-        )
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS locations(
-            id INTEGER PRIMARY KEY,
-            name STRING,
-            type STRING,
-            dimension STRING,
-            url STRING,
-            created DATETIME
-        )
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS episodes_characters(
-            id INTEGER PRIMARY KEY,
-            episode_id INTEGER,
-            character_id INTEGER
-        )
-    ''')
-
-
 def insert_episodes(episode_list: List):
-    rows_to_insert = [(episode['id'], episode['name'], episode['air_date'], episode['episode'], episode['url'],
-                       datetime.strptime(episode['created'], '%Y-%m-%dT%H:%M:%S.%fZ'))
-                      for episode in episode_list]
-    cursor.executemany('''
-        INSERT INTO episodes (id, name, air_date, episode, url, created)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', rows_to_insert)
-    con.commit()
+    pass
 
 
 def insert_characters(character_list: List):
-    # In the original format, the origin and the location fields are objects, not a single number, this
-    # comprehension list extracts the id from the url in the original format.
-    character_list = [{**row, 'origin_id': row['origin']['url'].split('/')[-1],
-                       'location_id': row['location']['url'].split('/')[-1]} for row in character_list]
-    for character in character_list:
-        character['origin_id'] = int(character['origin_id']) if character['origin_id'] else 0
-        character['location_id'] = int(character['location_id']) if character['location_id'] else 0
-
-    rows_to_insert = [(character['id'], character['name'], character['status'], character['species'],
-                       character['type'], character['gender'], character['image'], character['url'],
-                       datetime.strptime(character['created'], '%Y-%m-%dT%H:%M:%S.%fZ'),
-                       int(character['origin_id']), int(character['location_id'])) for character in character_list]
-    cursor.executemany('''
-        INSERT INTO characters (id, name, status, species, type, gender,
-        image, url, created, origin_id, location_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', rows_to_insert)
-    con.commit()
+    pass
 
 
 def insert_locations(location_list: List):
-    rows_to_insert = [(location['id'], location['name'], location['type'], location['dimension'],
-                       location['url'], datetime.strptime(location['created'], '%Y-%m-%dT%H:%M:%S.%fZ'))
-                      for location in location_list]
-    cursor.executemany('''
-        INSERT INTO locations (id, name, type, dimension, url, created)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', rows_to_insert)
-    con.commit()
+    pass
 
 
 def episode_characters_table(episode_list: List):
-    """Creates the table episode_characters in the database"""
-    rows_to_insert = []
-    for episode in episode_list:
-        characters_ids = [character.split('/')[-1] for character in episode['characters']]
-        episode_character_ids = [(episode['id'], int(character_id)) for character_id in characters_ids]
-        rows_to_insert.extend(episode_character_ids)
-
-    cursor.executemany('''
-       INSERT INTO episodes_characters (episode_id, character_id)
-        VALUES (?, ?)
-    ''', rows_to_insert)
-
-    con.commit()
+    pass
 
 
 def main():
+    # Creating the database
+    con = database_connection()
+    create_database(con)
+    con.close()
+    # Extracting the data from the API
     character_list, location_list, episode_list = extract_data()
-
-    create_database()
+    # Creating a new connection and the tables
+    con = database_connection(db_created=True)
+    create_tables(con)
+    # Inserting all the data into the tables
     # insert_episodes(episode_list)
     # insert_characters(character_list)
     # insert_locations(location_list)
@@ -157,5 +147,5 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         pass
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
